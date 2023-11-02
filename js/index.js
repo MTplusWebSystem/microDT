7aecedf799f887935be1559ee014b815f8285d75
MTplusWebSystem <mtpluswebsystem@gmail.com>
2023-11-02 18:01:35 -0400

    v0.0.1 alpha 2

--- js/index.js
+++ js/index.js
@@ -4,12 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
             const show = new Show();
             const view = document.querySelector(id);
             if (view) {
-                const list = ["#ctry", "#conf"];
-                show.itens(list);
-                view.style.transition = "0.6s";
+                const on = ["#ctry", "#conf","#sendmsg"];
+                const off = ["#start"];
+                show.enable(on);
+                show.disable(off)
+                view.style.transition = "0.3s";
                 view.style.height = ht;
                 view.style.width = wt;
-                start.id = "home_close";
                 return id;
             }
             return null;
@@ -33,20 +34,38 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     class Show {
-        itens(list) {
+        enable(list) {
             list.forEach(element => {
                 document.querySelector(element).style.display = "flex";
+            });
+        }
+        disable (list) {
+            list.forEach(element => {
+                document.querySelector(element).style.display = "none";
             });
         }
     }
 
     class App {
         constructor() {
-            const eventStart = new Event();
+            const show = new Show();
+            const eventClick = new Event();
             const render = new Render();
             const start = document.getElementById("start");
+            const creat_ctry = document.getElementById("ctry");
+            eventClick.click(start, () => render.open_vh("#home", "40vh", "60px"));
 
-            eventStart.click(start, () => render.open_vh("#home", "40vh", "60px"));
+            eventClick.click(creat_ctry, ()=> {
+                const off = [".infos",".notifications",".cardButtons",]
+                show.disable(off)
+            })
+            eventClick.click(creat_ctry, () =>{
+                render.open_vh("#home", "60px", "65px")
+                const on = ["#start"]
+                const off = ["#ctry", "#conf","#sendmsg"]
+                show.enable(on)
+                show.disable(off)
+            });
         }
     }
 
