eb5a3445c50a2021896edee07b8b701d79b7e700
MTplusWebSystem <mtpluswebsystem@gmail.com>
2023-11-04 13:11:03 -0400

    interfece da payload e categoria v0.2 feita

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
             element.addEventListener("click", callback);
         }
     }
-
+    
     class VisibilityManager {
         enable(elements) {
             elements.forEach(element => {
@@ -31,15 +31,62 @@ document.addEventListener("DOMContentLoaded", function() {
             const eventManager = new EventManager();
             const visibilityManager = new VisibilityManager();
             const conf = document.querySelector("#creatConfig");
+            const category = document.querySelector("#creatCategory");
             eventManager.click(conf, () => {
-                const offView = [".itensCard",".viewVersion",".viewNotas"];
+                const offView = [".itensCard",".viewVersion",".viewNotas",".viewCategory"];
                 const onView =[".viewConfig",".containerView"]
+                visibilityManager.disable(offView);
+                visibilityManager.enable(onView)
+            });
+            eventManager.click(category, () => {
+                const offView = [".itensCard",".viewVersion",".viewNotas",".viewConfig"];
+                const onView =[".containerView",".viewCategory"]
                 visibilityManager.disable(offView);
                 visibilityManager.enable(onView)
             });
         }
         
     }
+
+    const hexInput = document.getElementById('hex-input');
+    const redSlider = document.getElementById('red');
+    const greenSlider = document.getElementById('green');
+    const blueSlider = document.getElementById('blue');
+    const colorPreview = document.getElementById('color-preview');
+
+    function updateColor() {
+        const red = parseInt(redSlider.value);
+        const green = parseInt(greenSlider.value);
+        const blue = parseInt(blueSlider.value);
+        const hexValue = rgbToHex(red, green, blue);
+        colorPreview.style.backgroundColor = hexValue;
+        hexInput.value = hexValue;
+    }
+
+    function componentToHex(c) {
+        const hex = c.toString(16).toUpperCase();
+        return hex.length == 1 ? '0' + hex : hex;
+    }
+
+    function rgbToHex(r, g, b) {
+        return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+
+    hexInput.addEventListener('input', function() {
+        const hexValue = hexInput.value;
+        const isValidHex = /^#[0-9A-Fa-f]{6}$/g.test(hexValue);
+        if (isValidHex) {
+            const rgbValues = hexToRgb(hexValue);
+            redSlider.value = rgbValues.r;
+            greenSlider.value = rgbValues.g;
+            blueSlider.value = rgbValues.b;
+            colorPreview.style.backgroundColor = hexValue;
+        }
+    });
+
+    redSlider.addEventListener('input', updateColor);
+    greenSlider.addEventListener('input', updateColor);
+    blueSlider.addEventListener('input', updateColor);
 
     const Instance = new APP();
 });
